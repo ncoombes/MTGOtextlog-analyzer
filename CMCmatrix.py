b6c337cf232eef924c44890a15b9b60d0c13f676
ncoombes <47931327+ncoombes@users.noreply.github.com>
2019-02-26 08:11:34 -0800

    Update CMCmatrix.py
    changed name of cmc_list to color_dict

--- CMCmatrix.py
+++ CMCmatrix.py
@@ -6,7 +6,7 @@ for i,card in enumerate(RNAfullset):
     cardbase.loc[i,'cost']=card.mana_cost
     cardbase.loc[i,'name']=card.text
     
-cmc_list = ['W/B', 'B/R', 'W/U', 'G/U','R/G','B','G','U','W','R']
+color_dict = ['W/B', 'B/R', 'W/U', 'G/U','R/G','B','G','U','W','R']
 
 def replaceMultiple(mainString, toBeReplaces, newString):
     # Iterate over the strings to be replaced
@@ -18,9 +18,9 @@ def replaceMultiple(mainString, toBeReplaces, newString):
     
     return  mainString 
 
-#function for turning the mtgsdk mana cost column into generic converted mana cost( formated {num}{color}{color} etc. can handle hybrid mana of form {W/B} as long as cmc_list variable is properly defined earlier to include all 10 possible formats. This ignores variable costs of {x} for spells like fireball for now.
+#function for turning the mtgsdk mana cost column into generic converted mana cost( formated {num}{color}{color} etc. can handle hybrid mana of form {W/B} as long as color_dict variable is properly defined earlier to include all 10 possible formats. This ignores variable costs of {x} for spells like fireball for now.
 def cmc_col_gen (mana_cost):
-    mana_as_num=replaceMultiple(str(mana_cost), cmc_list, '1').replace('{','').replace('X','').replace('None','').split('}')
+    mana_as_num=replaceMultiple(str(mana_cost), color_dict, '1').replace('{','').replace('X','').replace('None','').split('}')
     cmc = sum( list(map(int,[x for x in mana_as_num if x] )) )
     return(cmc)
 
